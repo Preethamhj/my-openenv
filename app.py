084eff01ab9fb62cb9b0f0a322d83a79c7e243e2
preetham hj <hjpreetham1@gmail.com>
2026-04-07 23:09:56 +0530

    fix python path issue

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import sys
 import os
 
-# ✅ FIX import path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# ✅ CRITICAL FIX — add project root to path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from fastapi import FastAPI
-from env.environment import CyberEnv
+from env.environment import CyberEnv 
 
 app = FastAPI()
 env = CyberEnv()
